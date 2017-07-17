97d4d9a5701700eb3d28cd75cacf65b89919f560
masahiro hosoya <hosoya1985@gmail.com>
2017-07-17 20:48:06 +0900

    commit

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,6 +1,48 @@
 describe('LearnJS', function () {
     it('can show a problem view', function () {
         learnjs.showView('#problem-1');
-        exclude($('.view-container .problem-view').length).toEqual(1);
+        expect($('.view-container .problem-view').length).toEqual(1);
     });
+
+    it('show the landing page view when there is no hash', function () {
+        learnjs.showView('');
+        expect($('.view-container .landing-view').length).toEqual(1);
+    });
+
+    it('passes the hash view parameter to the view function', function () {
+        spyOn(learnjs, 'problemView');
+        learnjs.showView('#problem-42');
+        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+    });
+
+    describe('problem view', function () {
+        var view;
+        beforeEach(function () {
+            view = learnjs.problemView('1');
+        });
+        it('has a title that includes the problem number', function () {
+            expect(view.find('.title').text()).toEqual('Problem #1');
+        });
+
+        it('show the description', function () {
+            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+        });
+
+        it('show the problem code', function () {
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __ ; }');
+        });
+    });
+
+    it('invokes the router when loaded', function () {
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('subscribes to the hash change event', function () {
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    })
 });
